50eb06c33d887930cff3183676e233523e53ced5
Manuela Schrittwieser <67684505+MANU-de@users.noreply.github.com>
2024-04-05 10:55:58 +0200

    Add files via upload

---
+++ ArithmeticMean.java
@@ -0,0 +1,14 @@
+public class ArithmeticMean {
+    public static void main(String[]args){
+        int[] numbers ={1,2,3,4,5,6,1,2,3,4,5,6};
+        int sum = 0;
+
+        for (int n : numbers)
+        {
+            sum += n;
+        }
+
+        double mean = (double) sum / numbers.length;
+        System.out.println(mean);
+    }
+}
